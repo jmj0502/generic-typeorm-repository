34e6209865d6599b1a21c045d77fc9cf51588422
EliezerSPP <52340161+EliezerSPP@users.noreply.github.com>
2024-04-06 19:13:14 -0400

    docs(lib): Document each method and class in our library

--- lib/DecoratedEntity.ts
+++ lib/DecoratedEntity.ts
@@ -1,12 +1,11 @@
 import {
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  DeleteDateColumn,
-  UpdateDateColumn,
   Entity,
-  BaseEntity,
+  BaseEntity
 } from 'typeorm';
 
+/**
+ * @description This is our "base class" for all entities that will be used with our GenericRepository
+ */
 @Entity()
 export abstract class DecoratedEntity extends BaseEntity {
 }
